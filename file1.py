bb70ab23aeb1de38985a29b2bb8c465f4d011b3c
kowsalyaprasad98 <kowsalyaprasad98@gmail.com>
2022-12-08 11:56:33 +0530

    FirstCommit

---
+++ file1.py
@@ -0,0 +1,8 @@
+
+"""Number Of Products and total payment"""
+product1=int(input())
+product2=int(input())
+product3=int(input())
+totalamount=product1*20+product2*40+product3*40
+print(totalamount)
+
